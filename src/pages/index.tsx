b891928d4c5fb50bd0a0cab1cfdebe4459cd526e
kenleedevtop <kenleedevtop@gmail.com>
2022-11-12 23:42:44 +0100

    feature/PI-32-Card-with-text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { Title } from 'components/core';
 import { usePageContext } from 'context';
+import { CardWithText } from 'components/custom';
+import { Button } from '@mui/material';
 
 const Home = () => {
   const { setRouteName } = usePageContext();
@@ -13,6 +15,21 @@ const Home = () => {
     <>
       <Title>Home</Title>
       Home
+      <CardWithText
+        title="All influencers"
+        description="More than 290+ new Influencers"
+        actions={[
+          <Button color="secondary" variant="contained" size="small">
+            Filter
+          </Button>,
+          <Button color="default" variant="contained" size="small">
+            Export
+          </Button>,
+          <Button color="primary" variant="contained" size="small">
+            Add influencers
+          </Button>,
+        ]}
+      />
     </>
   );
 };
