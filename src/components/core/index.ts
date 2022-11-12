a8411af7e2a26551bbbf92ea6935c26870069c6b
kenleedevtop <kenleedevtop@gmail.com>
2022-11-12 16:52:04 +0100

    PI-29: Grid moved into system folder

--- src/components/core/index.ts
+++ src/components/core/index.ts
@@ -1,3 +1,2 @@
 export { default as Title } from 'components/core/title';
-export { default as Grid } from 'components/core/grid';
 export { default as PageLoader } from 'components/core/page-loader';
--- src/components/system/grid/index.tsx
+++ src/components/system/grid/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { GridMain } from 'components/core/grid/styles';
-import { TGridProps } from 'components/core/grid/types';
+import { GridMain } from 'components/system/grid/styles';
+import { TGridProps } from 'components/system/grid/types';
 
 const Grid = ({ ...props }: TGridProps) => <GridMain {...props} />;
 
