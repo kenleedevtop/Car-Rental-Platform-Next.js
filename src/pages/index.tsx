e7046e84b70593583bd86cf77e5d43a716fa5fe1
kenleedevtop <kenleedevtop@gmail.com>
2022-11-12 01:05:32 +0100

    PI-9: Input search icon added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Title } from 'components/core';
 import { Button, TextField } from 'components/ui';
 import { Navigation, Sidebar } from 'components/custom';
+import { SearchIcon } from 'components/svg';
 
 const Home = () => (
   <>
     <Title>Home</Title>
     <Navigation routeName="Home" />
     <Sidebar />
-    <TextField label="Full name" />
+    <TextField label="Full name" startIcon={<SearchIcon />} />
   </>
 );
 
