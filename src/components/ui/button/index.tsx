94a7c730e91b373a52d25571766644c1f1a49a54
kenleedevtop <kenleedevtop@gmail.com>
2022-11-15 14:33:43 +0100

    PI-9: Button changes

--- src/components/ui/button/index.tsx
+++ src/components/ui/button/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ButtonMain } from 'components/ui/button/styles';
+import { TButtonProps } from 'components/ui/button/types';
 
-const Button = ({ ...props }) => <ButtonMain {...props} />;
+const Button = ({ ...props }: TButtonProps) => <ButtonMain {...props} />;
 
 export default Button;
