46483cca885e23251ae9eeb3244bf279b29c843e
kenleedevtop <kenleedevtop@gmail.com>
2022-11-30 16:54:04 +0100

    PI-39: Input group

--- src/features/home/role/influencer/index.tsx
+++ src/features/home/role/influencer/index.tsx
@@ -1,5 +1,46 @@
-import React from 'react';
+import { CardWithText } from 'components/custom';
+import { InputGroup } from 'components/ui';
+import React, { useState } from 'react';
 
-const HomePage = () => <div>HomePage</div>;
+const HomePage = () => {
+  const [state, setState] = useState({
+    currency: null,
+    price: '',
+  });
+
+  return (
+    <CardWithText title="Home" description="This is home">
+      <InputGroup
+        label="Income"
+        inputRatio="100px 1fr "
+        required
+        elements={[
+          {
+            value: state.currency,
+            onValue: (currency) => setState({ ...state, currency }),
+            type: 'select',
+            placeholder: 'CUR',
+            options: [
+              {
+                value: 'eur',
+                label: 'EUR',
+              },
+              {
+                value: 'usd',
+                label: 'USD',
+              },
+            ],
+          },
+          {
+            value: state.price,
+            onValue: (price) => setState({ ...state, price }),
+            type: 'number',
+            placeholder: 'Price',
+          },
+        ]}
+      />
+    </CardWithText>
+  );
+};
 
 export default HomePage;
