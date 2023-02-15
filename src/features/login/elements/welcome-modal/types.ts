96cb6efa542412712a9d4975f0c92c2d06c4b130
kenleedevtop <kenleedevtop@gmail.com>
2023-02-15 16:31:13 +0100

    small welcome popup fix 2

--- src/features/login/elements/welcome-modal/types.ts
+++ src/features/login/elements/welcome-modal/types.ts
@@ -2,6 +2,6 @@ import React from 'react';
 
 export type TWelcomeModalProps = React.HTMLAttributes<HTMLDivElement> & {
   onClose: () => void;
-  role: 'INFLUENCER' | 'COMPANY' | null;
+  role: 'INFLUENCER' | 'CLIENT' | null;
   error: boolean;
 };
