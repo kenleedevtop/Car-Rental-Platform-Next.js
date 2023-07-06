6e5cad21a202c45faea0fd5857aff6b36dbef58f
kenleedevtop <kenleedevtop@gmail.com>
2023-07-06 10:35:39 +0200

    add s3 bucket domain

--- next.config.js
+++ next.config.js
@@ -5,4 +5,7 @@ const { i18n } = require('./next-i18next.config');
 module.exports = withFonts({
   reactStrictMode: true,
   i18n,
+  images: {
+    domains: ['patients-influence-dev.s3.eu-central-1.amazonaws.com'],
+  },
 });
